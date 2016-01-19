549ba2284c95f14ee500c0fa79b6a35e6f401704
Gerard Beckerleg <gerard.beckerleg@orchard.com.au>
2016-01-19 15:15:34 +1100

    add client

---
+++ SecureClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SecureClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new Service1Client();
+
+            //System.Net.ServicePointManager.ServerCertificateValidationCallback +=
+            //    (se, cert, chain, sslerror) =>
+            //    {
+            //        return true;
+            //    };
+
+            var result = client.GetData(1);
+
+            Console.WriteLine(result);
+        }
+    }
+}
+
